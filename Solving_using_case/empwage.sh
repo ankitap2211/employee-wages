5d3d5ea721c30b8cb1af00e785031378388372dc
ankitap2211 <ankitapatil3166@gmail.com>
2022-05-02 14:47:36 +0530

    Solving using case

---
+++ Solving_using_case/empwage.sh
@@ -0,0 +1,19 @@
+attendence=$(( RANDOM % 3 ))
+case $attendence in 
+    0)
+        echo "Employee is absent"
+        no_of_hrs_per_day=0
+        ;;
+    1)
+        echo "Employee is full time present"
+        no_of_hrs_per_day=8
+        ;;
+    2)
+        echo "Employee is part time present"
+        no_of_hrs_per_day=4
+        ;;
+esac
+
+wage_per_hr=20
+wage_per_day=$(( wage_per_hr * no_of_hrs_per_day ))
+echo "The daily wage of the employee is $wage_per_day "
